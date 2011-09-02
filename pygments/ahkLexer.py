24bf56ca94c478534d26bf93915e77f9b05f6a9b
Naveen Garg <naveen.garg@gmail.com>
2011-09-02 15:58:18 -0500

    Labels Hotkeys HotStrings

--- pygments/ahkLexer.py
+++ pygments/ahkLexer.py
@@ -1,5 +1,6 @@
 from pygments.lexer import RegexLexer, bygroups, using, include
 from pygments.token import *   
+import re
 from pygments.styles import get_style_by_name
 ahkcommandsFile = 'ahkSyntax/CommandNames.txt'
 f = open(ahkcommandsFile)
@@ -9,7 +10,8 @@ f.close()
 class AhkLexer(RegexLexer):    	    
     name = 'AHK'	       	    
     aliases = ['ahk']	       	
-    filenames = ['*.ahk', '*.ahk2']   
+    filenames = ['*.ahk', '*.ahk2']
+#    flags = re.IGNORECASE  use (?i) in commands only 
     tokens = { 		       	
         'root': [	       
             (r'^(\s*)(/\*)', bygroups(Whitespace, Comment.Multiline),
@@ -18,6 +20,8 @@ class AhkLexer(RegexLexer):
             (r'\s+;.*?$', Comment.Singleline),
             (r'^;.*?$', Comment.Singleline),
             include('commands'),
+            include('labels'),
+            include('numbers'),
             include('garbage'),
         ],		       
         'incomment': [	       
@@ -31,8 +35,23 @@ class AhkLexer(RegexLexer):
 	    (r'[)]', Generic),
 	    ],
         'commands': [
-            (r'^(\s*)(' + ahkcommands + r')\b', bygroups(Whitespace,  Keyword))
+            (r'(?i)^(\s*)(' + ahkcommands + r')\b', bygroups(Whitespace,  Keyword))
             ],
+        'labels': [
+            # hotkeys and labels 
+            # technically, hotkey names are limited to named keys and buttons
+            (r'(^\s*)([^:\s]+?:{1,2})', bygroups(Whitespace, Name.Label)), 
+            (r'(^\s*)(::[^:\s]+?::)', bygroups(Whitespace, Name.Label)),  
+            ],
+        
+        'numbers': [
+            (r'(\d+\.\d*|\d*\.\d+)([eE][+-]?[0-9]+)?', Number.Float),
+            (r'\d+[eE][+-]?[0-9]+', Number.Float),
+            (r'0\d+', Number.Oct),
+            (r'0[xX][a-fA-F0-9]+', Number.Hex),
+            (r'\d+L', Number.Integer.Long),
+            (r'\d+', Number.Integer)
+        ],        
         'garbage': [
             (r'[^\S\n]', Text),
             (r'/', Text),      
--- pygments/sample1.ahk
+++ pygments/sample1.ahk
@@ -1,6 +1,11 @@
  ; single line comment
 ; single line comment
 #NoTrayIcon  ; single line comment
+k::
+::sdf::
+label:
+Msgbox
+Return
 /*  multiline
 comment
 */
