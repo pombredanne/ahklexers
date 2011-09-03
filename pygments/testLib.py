11bddbfdd28948a3635d1fabf3fda64f0d55f735
Naveen Garg <naveen.garg@gmail.com>
2011-09-03 12:37:44 -0500

    compare pygments1.4 autohotkeyLexer to new ahklexer

--- pygments/testLib.py
+++ pygments/testLib.py
@@ -1,4 +1,5 @@
 import os, sys, subprocess, re, codecs
+from pygments.lexers import AutohotkeyLexer
 from ahkLexer import AhkLexer 
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
@@ -7,17 +8,20 @@ from pygments.token import *
 import os, os.path, glob
 samplesDir = 'samples/stdlib/'
 for f in glob.glob( os.path.join(samplesDir, '*.ahk') ):
-    print "current file is: " + f
+#    print "current file is: " + f
     fh = open(f, 'r')
     code = fh.read()
     fh.close()			   				 
     formatter = HtmlFormatter() 
     codeh = highlight(code, AhkLexer(), formatter)
+    codeo = highlight(code, AutohotkeyLexer(), formatter)
     if(-1 != codeh.find('class="err"')):
-        print "failed on file {0}".format(f,)
+        print "new lexer failed on file {0}".format(f,)
+    if(-1 != codeo.find('class="err"')):
+        print "old lexer failed on file {0}".format(f,)
     g = f.replace("ahk", "html")
     gh = codecs.open(g, 'w', 'utf-8')
     linktag = '''<LINK href="styles.css" rel="stylesheet" type="text/css">'''
-    gh.write(linktag + codeh)
+    gh.write(linktag + "old lexer: \n" + codeo + "\n\nNew Lexer: \n" + codeh)
     gh.close()
 
